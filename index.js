aeb4966c45469ea4454666fb6f788d8e53305fd5
roych98 <roychorley1998@gmail.com>
2024-03-03 16:23:16 +0200

    Add a rust server config, cli functionality

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 require('dotenv').config();
+const _ = require('lodash');
 const RustPlus = require('@liamcottle/rustplus.js');
 const { Client, GatewayIntentBits, Partials, EmbedBuilder, ChannelType, ActionRowBuilder, ButtonBuilder, ButtonStyle, PermissionsBitField } = require('discord.js');
 const Bot = require('./utils/Bot');
 
+const providersConfig = require('./rust.servers.config');
+
 const discordBot = new Bot();
 
 (async (args) => {
-  const serverProvider = args[0];
-  const serverType = args[1];
+  const serverProvider = args[2];
+  const serverType = args[3];
 
-  const providerConfig = _.get(require('./rust.servers.config.json'), `${serverProvider}`);
+  const availableProviders = _.keys(providersConfig);
+  if (!_.includes(availableProviders, serverProvider)) {
+    console.error(`Bad provider: syntax: npm start <${_.join(availableProviders, ', ')}>`);
+    return;
+  }
+
+  const providerConfig = providersConfig[serverProvider];
+  const availableServers = _.keys(providerConfig.servers);
+
+  if (!_.includes(availableServers, serverType)) {
+    console.error(`Bad server type: syntax: npm start <${_.join(availableServers, ', ')}>`);
+    return;
+  }
+
   const serverConfig = _.get(providerConfig.servers, `${serverType}`);
   try {
     const rustplus = new RustPlus(serverConfig.ip, serverConfig.port, process.env.PLAYER_ID, process.env.PLAYER_TOKEN);
@@ -21,5 +37,7 @@ const discordBot = new Bot();
     // connect to rust server
     rustplus.connect();
   } catch (e) {
+    console.error(e);
+    process.exit(1);
   }
-})(process.env.args);
+})(process.argv);
---
+++ rust.servers.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  rustoria: {
+    day: 45,
+    night: 15,
+    servers: {
+      eu_main: {
+        ip: '123.123.123.123',
+        port: '27015'
+      }
+    }
+  },
+  rustymoose: {
+    day: 45,
+    night: 15,
+    servers: {
+      eu_main: {
+        ip: '123.123.123.123',
+        port: '27015'
+      }
+    }
+  }
+};
